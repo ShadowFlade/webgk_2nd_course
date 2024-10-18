5c2aea4a470364bc1b8709e6e0a0152a8fce447c
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-10-18 23:08:15 +0300

    master:elements are being created

--- local/php_interface/webgk/lib/Service/CatalogSyncService.php
+++ local/php_interface/webgk/lib/Service/CatalogSyncService.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Webgk\Service;
+
+use Bitrix\Iblock\ElementTable;
+use Bitrix\Iblock\PropertyTable;
+
 class CatalogSyncService
 {
     private $GOODS_IB_ID_IN;
@@ -14,7 +18,7 @@ class CatalogSyncService
         'CATALOGUE' => ['PROPERTY_TYPE' => 'S', 'USER_TYPE' => 'directory'],
         'STRING' => ['PROPERTY_TYPE' => 'S', 'USER_TYPE' => ''],
         'FILE' => ['PROPERTY_TYPE' => 'F', 'USER_TYPE' => ''],
-    ];
+    ];//those are the types we handle 
 
     public function __construct($GOODS_IB_ID_IN, $GOODS_TP_IB_ID_IN, $GOODS_IB_ID_OUT, $GOODS_TP_IB_ID_OUT)
     {
@@ -31,47 +35,78 @@ class CatalogSyncService
     {
         $els = \Bitrix\Iblock\ElementTable::getList([
             'filter' => ['IBLOCK_ID' => $this->GOODS_IB_ID_IN],
-            'select' => ['ID', 'XML_ID']
+            'select' => ['ID', 'XML_ID', 'CODE']
         ])->fetchAll();
-        $this->createProps();
+        $existingElsDB = \Bitrix\Iblock\ElementTable::getList([
+            'filter' => ['IBLOCK_ID' => $this->GOODS_IB_ID_OUT],
+            'select' => ['ID', 'XML_ID', 'CODE']
+        ]);
+
+        while ($existingEl = $existingElsDB->fetch()) {
+            $existingEls[$existingEl['CODE']] = $existingEl;
+        }
+        \Bitrix\Main\Diag\Debug::writeToFile($existingEls, date("d.m.Y H:i:s"), "local/log.log");
+
+
+//        $this->createProps();
+        $createdCount = 0;
+        $updatedCount = 0;
 
         foreach ($els as $el) {
             $element = \CIBlockElement::GetByID($el['ID'])->GetNextElement();
             $fields = $element->GetFields();
+
             $props = $element->GetProperties();
             $newEl = new \CIBlockElement();
-            $isThisElExists = \Bitrix\Iblock\ElementTable::getList([
-                    'filter' => ['IBLOCK_ID' => $this->GOODS_IB_ID_OUT, 'XML_ID' => $el['XML_ID']],
-                    'select' => ['ID', 'XML_ID']
-                ])->getSelectedRowsCount() > 0;
+            $isThisElExists = isset($existingEls[$el['CODE']]);
+            $allProps = $this->formatPropsForAddingUpdating($props);
+//            $newFields = $fields;
+            $newFields = [
+                'IBLOCK_ID' => $this->GOODS_IB_ID_OUT,
+                'NAME' => $fields['NAME'],
+                'CODE' => $fields['CODE'],
+                'PROPERTY_VALUES' => $allProps
+            ];
+//            $newProps['IBLOCK_ID'] = $this->GOODS_IB_ID_OUT;
+//            unset(
+//                $newProps['~IBLOCK_ID'], $newProps['DATE_CREATE_UNIX'], $newProps['~DATE_CREATE_UNIX'],
+//                $newProps['TIMESTAMP_X'], $newProps['~TIMESTAMP_X'], $newProps['TIMESTAMP_X_UNIX'], $newProps['~TIMESTAMP_X_UNIX'],
+//                $newProps['IBLOCK_SECTION_ID'], //TODO for now - later change to needed group id
+//                $newProps['IBLOCK_EXTERNAL_ID'],
+//                $newProps['~IBLOCK_EXTERNAL_ID'],
+//                $newProps['CREATED_DATE'],
+//                $newProps['~CREATED_DATE'],
+//            );
+
             if ($isThisElExists) {
-                $allProps = [];
-                foreach ($props as $prop) {
 
-                    $propertyValues = [];
-                    if (is_array($prop['VALUE'])) {
-                        $propValNew = [];
-                        foreach ($prop['VALUE'] as $key => $propVal) {
-                            $propValNew['VALUE'] = $propVal;
-                            $propValNew['DESCRIPTION'] = $prop['DESCRIPTION'][$key];
-                            $propertyValues[] = $propValNew;
-                        }
 
-                    } else {
-                        $propertyValues = $prop['VALUE'];
-                    }
-                    $propVals = $propertyValues;
-                    $allProps[$prop['CODE']] = $propVals;
-                }
-                $newEl->Update(
-                    $fields['ID'],
-                    array_merge(
-                        $fields,
-                        ['PROPERTY_VALUES' => $allProps]
-                    )
+                $isUpdated = $newEl->Update(
+                    $existingEls[$el['CODE']]['ID'],
+                    $newFields
                 );
+                if (!$isUpdated) {
+                    \Bitrix\Main\Diag\Debug::writeToFile(['not updated' => $newEl->LAST_ERROR], date("d.m.Y H:i:s"), "local/log.log");
+
+                } else {
+                    $updatedCount++;
+
+                }
+            } else {
+                $id = $newEl->Add(
+                    $newFields
+                );
+                if ($id === false) {
+                    \Bitrix\Main\Diag\Debug::writeToFile(['not created ' => $newEl->LAST_ERROR], date("d.m.Y H:i:s"), "local/log.log");
+
+                } else {
+                    $createdCount++;
+
+                }
             }
         }
+        \Bitrix\Main\Diag\Debug::writeToFile(['created' => $createdCount, ' updated' => $updatedCount], date("d.m.Y H:i:s"), "local/log.log");
+
     }
 
     public function createProps()
@@ -79,6 +114,12 @@ class CatalogSyncService
         $propsCount = 0;
 
         $propsDB = \CIBlockProperty::GetList([], ["IBLOCK_ID" => $this->GOODS_IB_ID_IN]);
+        $existingPropsInNewCatalogDB = PropertyTable::getList(['filter' => ['IBLOCK_ID' => $this->GOODS_IB_ID_OUT], 'select' => ['NAME', 'ID', 'CODE']]);
+
+        while ($existingProp = $existingPropsInNewCatalogDB->fetch()) {
+            $existingProps[$existingProp['CODE']] = $existingProp;
+        }
+
 
         while ($prop = $propsDB->GetNext()) {
             $props[] = $prop;
@@ -90,6 +131,11 @@ class CatalogSyncService
 
 
         foreach ($props as $prop) {
+
+            if (isset($existingProps[$prop['CODE']])) {//if prop with such code already exists skip it - maybe change it to configurable later
+                continue;
+            }
+
             $newProp = new \CIBlockProperty();
             unset($prop['ID']);
             unset($prop['TMP_ID']);
@@ -131,6 +177,8 @@ class CatalogSyncService
             $propsCount++;
 
         }
+        \Bitrix\Main\Diag\Debug::writeToFile(['props created' => $propsCount], date("d.m.Y H:i:s"), "local/log.log");
+
         return $propsCount;
     }
 
@@ -143,13 +191,36 @@ class CatalogSyncService
         while ($property = $propertyRes->Fetch()) {
             $propertyId = $property['ID'];
 
-            if (CIBlockProperty::Delete($propertyId)) {
+            if (\CIBlockProperty::Delete($propertyId)) {
                 $propsDeleted++;
             } else {
                 $propsErrors++;
             }
         }
         return [$propsDeleted, $propsErrors];
+    }
+
+    private function formatPropsForAddingUpdating($props)
+    {
+        $allProps = [];
+        foreach ($props as $prop) {
+
+            $propertyValues = [];
+            if (is_array($prop['VALUE'])) {
+                $propValNew = [];
+                foreach ($prop['VALUE'] as $key => $propVal) {
+                    $propValNew['VALUE'] = $propVal;
+                    $propValNew['DESCRIPTION'] = $prop['DESCRIPTION'][$key];
+                    $propertyValues[] = $propValNew;
+                }
+
+            } else {
+                $propertyValues = $prop['VALUE'];
+            }
+            $propVals = $propertyValues;
+            $allProps[$prop['CODE']] = $propVals;
+        }
+        return $allProps;
     }
 
     private function syncTpCatalog()
@@ -186,7 +257,9 @@ class CatalogSyncService
     private function createCatalogueProp($fields)
     {
         $newFields = $fields;
-
     }
 
 }
+
+//TODO
+//1. bug in the created element it creates Костюм Футболка/шорты Smaillook &amp;amp;amp;quot;Зажигаю солнце&amp;amp;amp;quot; малодетский like this (with &amp;)
