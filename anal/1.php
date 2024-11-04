eb7d43954f03b0dcbac646977dfc31f951680abe
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-11-05 02:27:38 +0300

    master[12]:added analitycs

---
+++ anal/1.php
@@ -0,0 +1,35 @@
+<?
+//ajax обработчик
+
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+
+CModule::IncludeModule("sale");
+$arSelect = array("ID", "IBLOCK_ID", "NAME", "PREVIEW_PICTURE", "PROPERTY_MORE_PHOTO");
+$arFilter = array("IBLOCK_ID" => 16, "ID" => $_POST["id"]);
+$res = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+$arIDs = [];
+while ($ob = $res->GetNextElement()) {
+    $arFields = $ob->GetFields();
+    if ($arFields["PREVIEW_PICTURE"]) $arIDs[$arFields["PREVIEW_PICTURE"]] = 1;
+    if ($arFields["PROPERTY_MORE_PHOTO_VALUE"]) $arIDs[$arFields["PROPERTY_MORE_PHOTO_VALUE"]] = 1;
+}
+
+$arImgs = [];
+foreach ($arIDs as $key => $val) {
+    if (isset($_POST['size'])) {
+        $arImgs[] = resizeImageByWidth($key, $_POST['size']);
+    } else {
+        $arImgs[] = CFile::GetPath($key);
+    }
+
+}
+
+$result = array(
+    'result' => 'OK',
+    'id' => $_POST["id"],
+    'imgs' => $arImgs
+);
+
+echo json_encode($result);
+
